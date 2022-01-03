6534ea322ac3c7016982b710d461c00bcd4e243b
viachaskul <fixrapdoK@gmail.com>
2022-01-03 18:07:03 +0300

    init: Start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    //function reverse(n) {
+
+    ((n < 1) ? n = n * -1 : n = n * 1); //if negative
+    n = n.toString().split("").reverse().join(""); //reverse string
+    return Number(n.replace(/^0*/, "")); // return number without zeros
 }
+
+//console.log(reverse(-1230));
